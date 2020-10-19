88683207508a96913cc12b99351adc3bffc2a0be
Daniel S. Roche <dsroche@gmail.com>
2020-10-19 02:14:26 -0400

    hw22 starter

---
+++ hw22/pairs.c
@@ -0,0 +1,4 @@
+/* pairs.c: Read in x and y values and pair them up.
+ * Erase this comment and put your name here!
+ * Your actual code should start below.
+ */
---
+++ hw22/substring.c
@@ -0,0 +1,4 @@
+/* substring.c: Read two strings and determine whether one contains the other.
+ * Erase this comment and put your name here!
+ * Your actual code should start below.
+ */
